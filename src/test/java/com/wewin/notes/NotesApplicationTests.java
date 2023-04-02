22069a4b22c840adb91c100ad79bdc29e81daff4
wewineitor <gamerisaac48@gmail.com>
2023-04-02 14:38:02 -0600

    Configuracion incial del proyecto

---
+++ src/test/java/com/wewin/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wewin.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
