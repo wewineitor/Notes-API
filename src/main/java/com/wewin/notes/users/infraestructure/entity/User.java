d7e2346477c739dd651beba5a157330d4da2f1e4
wewineitor <gamerisaac48@gmail.com>
2023-04-02 15:03:12 -0600

    Agregada capa de infraestructura para usuarios

---
+++ src/main/java/com/wewin/notes/users/infraestructure/entity/User.java
@@ -0,0 +1,20 @@
+package com.wewin.notes.users.infraestructure.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String password;
+    private String username;
+    private String email;
+}
---
+++ src/main/java/com/wewin/notes/users/infraestructure/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.wewin.notes.users.infraestructure.repository;
+
+import com.wewin.notes.users.infraestructure.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
