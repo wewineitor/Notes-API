59cb39e8b5905c7664a760b2e37c9ce44db6851f
wewineitor <gamerisaac48@gmail.com>
2023-04-02 21:33:47 -0600

    Eliminado import innecesario

--- src/main/java/com/wewin/notes/users/application/controller/UserController.java
+++ src/main/java/com/wewin/notes/users/application/controller/UserController.java
@@ -4,7 +4,6 @@ import com.wewin.notes.users.domain.dto.UserDTO;
 import com.wewin.notes.users.domain.port.UserServicePort;
 import com.wewin.notes.users.infraestructure.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
